5b9f7228d2a13d516e72c12d8881e7844c54c0e0
Erin Walker <ewalker@ewalkerblog.co.za>
2012-07-09 10:57:59 +0200

    first_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mypage::Application.config.secret_token = '94951703bf250b1f32cc110480f03ed4dbc62d37484c63ecbc455f54b6fe9a2530cb4d6c4eb7f1784bbcaf2e266e4b036f2261a94f9cef93e2cebb1cd2ad8405'
